69bb4791745c81782f6c99c73f3b30414acf6801
yzq <809145623@qq.com>
2016-07-07 13:46:07 +0800

    作品修改

--- js/myjs.js
+++ js/myjs.js
@@ -17,3 +17,20 @@ function browserRedirect() {
         window.location.href="http://m.blog.csdn.net/blog/index?username=leafact";
     }
 }
+
+function browserZhangHaoGuanJiaRedirect() {
+    var sUserAgent = navigator.userAgent.toLowerCase();
+    var bIsIpad = sUserAgent.match(/ipad/i) == "ipad";
+    var bIsIphoneOs = sUserAgent.match(/iphone os/i) == "iphone os";
+    var bIsMidp = sUserAgent.match(/midp/i) == "midp";
+    var bIsUc7 = sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4";
+    var bIsUc = sUserAgent.match(/ucweb/i) == "ucweb";
+    var bIsAndroid = sUserAgent.match(/android/i) == "android";
+    var bIsCE = sUserAgent.match(/windows ce/i) == "windows ce";
+    var bIsWM = sUserAgent.match(/windows mobile/i) == "windows mobile";
+    if (!(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM) ){
+        window.location.href="http://android.myapp.com/myapp/detail.htm?apkName=com.leafact.helppwd";
+    } else {
+        window.location.href="http://a.app.qq.com/o/simple.jsp?pkgname=com.leafact.helppwd";
+    }
+}
